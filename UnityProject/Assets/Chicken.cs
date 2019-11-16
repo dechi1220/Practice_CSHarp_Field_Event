8877027977bd4c6f42a4e859c5bc98bc11681948
dechi1220 <57282811+dechi1220@users.noreply.github.com>
2019-11-16 12:40:36 +0800

    Dechi_P2
    新增C#

---
+++ UnityProject/Assets/Chicken.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+  
+
+    [Header("移動速度"), Range(1, 100)]
+    public int speed = 1;
+
+    [Header("跳躍高度"), Range(1, 2000)]
+    public float jump = 1;
+    [Header("對話內容")]
+        public string talk = "咕咕";
+    [Header("是否取得雞蛋")]
+    public bool mission = true;
+
+    public int count = 0;
+
+    private void Start()
+    {
+        print("遊戲開始!!!");
+    }
+
+    private void Update()
+    {
+        print("咕咕咕咕咕咕咕咕咕咕咕");
+
+    }
+
+}
+
+  
